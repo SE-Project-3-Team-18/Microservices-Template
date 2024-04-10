4680d63129cf91b0edd0a66f744c4227e26d7fd5
Abhinav Reddy <abhinavreddyboddu@gmail.com>
2024-04-10 11:01:55 +0530

    added comments for service registry

--- src/utils/serviceRegistry.js
+++ src/utils/serviceRegistry.js
@@ -2,6 +2,22 @@ const axios = require('axios');
 const config = require('../config/config');
 const { CustomError } = require('./error');
 
+/**
+ * A class to abstract away interactions with the service registry.
+ * When initialising,
+ *  - first create new Object Instance
+ *  - then use .initialise()
+ * Note: Initialisation must be done only once.
+ *
+ * This class must be singleton. so initialise it only once,
+ * and when you want to use it later, use .getInstance() to
+ * fetch the same initialised instance.
+ *
+ * Initialisation is done in index.js
+ *
+ * usable functions:
+ * - getUrl: get the url of a service, given the service name.
+ */
 class ServiceRegistryClient {
   static instance = null;
 
